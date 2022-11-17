75ab4dde176facee8988975fac8888fdbccd6756
Richie-Yin <richiesyin@icloud.com>
2022-11-17 12:48:07 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function incrementButton1() {
+    var element = document.getElementById ('incrementtext1');
+    var value = element.innerHTML
+    ++value;
+    console.log(value);
+document.getElementById('incrementtext1').innerHTML = value;
+}
+
+function incrementButton2() {
+    var element = document.getElementById ('incrementtext2');
+    var value = element.innerHTML
+    ++value;
+    console.log(value);
+document.getElementById('incrementtext2').innerHTML = value;
+}
+
+function incrementButton3() {
+    var element = document.getElementById ('incrementtext3');
+    var value = element.innerHTML
+    ++value;
+    console.log(value);
+document.getElementById('incrementtext3').innerHTML = value;
+}
